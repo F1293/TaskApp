38feffdc7fda27ed2fe266d0a917fd2d5ef35531
F1293 <31593379+F1293@users.noreply.github.com>
2017-09-26 00:03:47 +0900

    タスク管理アプリ

---
+++ app/src/main/java/jp/techacademy/fumio/ueda/taskapp/TaskApp.java
@@ -0,0 +1,14 @@
+package jp.techacademy.fumio.ueda.taskapp;
+
+
+import android.app.Application;
+
+import io.realm.Realm;
+
+public class TaskApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Realm.init(this);
+    }
+}
